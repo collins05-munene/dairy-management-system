03af63df874d01be1a05a4068c88c1c2a6be905a
munenecollins <collinsgitonga296@gmail.com>
2026-05-15 17:50:01 +0300

    Worked on feed inventory models

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'inventory',
     'users',
 
     'rest_framework'
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
